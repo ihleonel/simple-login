b8f1e5d5eeb6a9185680e9fa8b4f82ad6f4528a0
ihleonel <ibarra.h.leonel@gmail.com>
2024-08-29 19:31:35 -0300

    Refactor login applying clean architecture

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ export default function Login() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const submit = () => {
-    fetch('http://localhost:8000/api/token/', {
+    fetch('http://localhost:8000/auth/api/token/', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {
