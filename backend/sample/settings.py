04619f247d7d20e638adb1ef35e4d2edc2005a20
ihleonel <ibarra.h.leonel@gmail.com>
2024-09-01 20:03:18 -0300

    Add posgresql database service

--- backend/sample/settings.py
+++ backend/sample/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sampledb',
+        'USER': 'userdb',
+        'PASSWORD': 'secretdb',
+        'HOST': 'db',
+        'POST': '5432',
     }
 }
 
