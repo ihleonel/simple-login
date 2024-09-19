57a25fca75b5876d7840871d035b15a9eb69b9ac
ihleonel <ibarra.h.leonel@gmail.com>
2024-09-18 23:21:26 -0300

    Appliging linting

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from 'wouter'
-import ProtectedRoute from './ProtectedRoute'
-import Login from '../pages/Login'
 import Home from '../pages/Home'
 import Dashboard from '../pages/Dashboard'
+import Login from '../pages/Login'
+import ProtectedRoute from './ProtectedRoute'
 export default function AppRoutes() {
   return (
     <Switch>
