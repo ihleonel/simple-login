bdceed790c7634e88be16306409661af97f96944
ihleonel <ibarra.h.leonel@gmail.com>
2024-09-24 22:30:58 -0300

    Indentation

--- backend/auth/infrastructure/views.py
+++ backend/auth/infrastructure/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
-from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 from auth.domain.user import User
 
 
